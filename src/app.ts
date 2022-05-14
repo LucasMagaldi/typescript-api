cc6599d289a5c02dc8d68ea0807fcf93bb1daa7d
LucasMagaldi <lucas.magaldi@hotmail.com>
2022-05-14 12:40:52 -0300

    Express config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello World TS")
+import express, { Express } from 'express';
+import cors from 'cors';
+
+class App {
+
+    server
+
+    constructor() {
+        this.server = express();
+    }
+}
+
+
+
+export default new App().server
